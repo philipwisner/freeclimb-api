cd7043d29a2871cec5b7e6f15e6bcf5eaeb817bc
Philip Wisner <philipwisner@gmail.com>
2019-11-18 22:57:21 -0500

    api

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
 const express = require('express');
 const app = express();
+require("dotenv").config();
 const freeclimbSDK = require('@freeclimb/sdk');
 const bodyParser = require("body-parser");
-var port = process.env.PORT || 80;
+const port = process.env.PORT || 80;
 
-// your freeclimb API key (available in the Dashboard) - be sure to set up environment variables to store these values
 const accountId = process.env.accountId;
 const authToken = process.env.authToken;
-const freeclimb = freeclimbSDK(accountId, authToken);
+const fc = freeclimbSDK(accountId, authToken);
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/submit", (req, res) => {
-  console.log("req", req.body);
   sendTextMessage(req.body.name, req.body.number, req.body.message);
-  return res.status(200);
+  return res.redirect('/');
 });
+
+app.post("/sms", (req, res) => {
+  return res.status(200);
+})
 
 function sendTextMessage(name, number, message) {
   var message = name + ' has contacted you. ' + 'Message: ' + message + ". Please contact them at " + number + "."
-  //Need to send a text with all the info to my phone number from bought number
-  console.log(message);
- /* app.post("/submit", (req, res) => {
-    console.log("req", req.body);
-    //console.log("res", res);
-    return res.status(200);
-  }); */
+  var to = "+12156068579";
+  var from = "+12152406199";
+  return fc.api.messages.create(from, to, message);
 }
 
 app.listen(port);
--- test.js
+++
@@ -1,9 +0,0 @@
-var freeClimbSDK = require("@freeclimb/sdk");
-require("dotenv").config();
-const accountId = process.env.accountId;
-const authToken = process.env.authToken;
-var fc = freeClimbSDK(accountId, authToken);
-var to = "+12156068579";
-var from = "+12152406199";
-
-fc.api.messages.create(from, to, "Welcome to FreeClimb!");
